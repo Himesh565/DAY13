d0f6f7d898c58b797b2970c9e6ab150544dbd9ce
Himesh Ambaliya <158713503+Himesh565@users.noreply.github.com>
2025-12-18 18:10:16 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -58,10 +58,8 @@ app.get('/', (req, res) => {
 // Start Server
 // Start Server
 const PORT = process.env.PORT || 5000;
-if (process.env.NODE_ENV !== 'production') {
-    app.listen(PORT, () => {
-        console.log(`🚀 Server is running on http://localhost:${PORT}`);
-    });
-}
+app.listen(PORT, () => {
+    console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
 
 module.exports = app;
