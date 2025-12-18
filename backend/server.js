e3d68aa7da460d2dddcc6ffd8aadac3373afa1fd
Himesh Ambaliya <158713503+Himesh565@users.noreply.github.com>
2025-12-18 15:30:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -61,6 +61,7 @@ app.get('/', (req, res) => {
 });
 
 // Start Server
+// Start Server
 const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV !== 'production') {
     app.listen(PORT, () => {
